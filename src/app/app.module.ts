e071d8879b967dceae958184fcc73f0286bd77c1
Gregorio Michalopulos <gmichalopulos@baufest.com>
2019-06-01 23:40:37 -0300

    Assigment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { ContactComponent } from './contact/contact.component';
 //Services
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 
 @NgModule({
@@ -49,7 +50,7 @@ import { PromotionService } from './services/promotion.service';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
